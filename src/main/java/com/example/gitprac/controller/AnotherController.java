961772f8ae9747bb4efb7612e5844ebc324a92f4
prabesh <prabesh.codehimalaya@gmail.com>
2024-01-19 15:39:34 +0545

    deone

--- src/main/java/com/example/gitprac/controller/AnotherController.java
+++ src/main/java/com/example/gitprac/controller/AnotherController.java
@@ -1,6 +1,11 @@
 package com.example.gitprac.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AnotherController {
+    @GetMapping(path = "/home")
     public String home(){
         return "home";
     }
