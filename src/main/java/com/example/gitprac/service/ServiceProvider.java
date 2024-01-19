6d7c596728163c279353f6914e4d92eaf2fbe18d
prabesh <prabesh.codehimalaya@gmail.com>
2024-01-19 15:35:52 +0545

    mistake

--- src/main/java/com/example/gitprac/service/ServiceProvider.java
+++ src/main/java/com/example/gitprac/service/ServiceProvider.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class ServiceProvider {
     public String getSomething(){
-        return "edited";
+        return "editedgit switch";
     }
 }
