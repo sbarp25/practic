d2e3b39a89cf69462abb8b4b193874aa3b2e611a
prabesh <prabesh.codehimalaya@gmail.com>
2024-01-19 11:59:02 +0545

    create one

---
+++ src/main/java/com/example/gitprac/GitpracApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitprac/GitpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
